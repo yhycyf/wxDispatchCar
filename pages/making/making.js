386d5db90e5cf7b8a3b5640c06ad79da7d3b568c
taomiaoer <taomiaoer@163.com>
2021-02-08 11:14:50 +0800

    初始化项目

---
+++ pages/making/making.js
@@ -0,0 +1,30 @@
+/*
+ * @Author: your name
+ * @Date: 2021-02-07 09:12:55
+ * @LastEditTime: 2021-02-07 14:19:53
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \Scooter\pages\index\index.js
+ */
+// index.js
+// 获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: 'Helelo World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    active: 1,
+  },
+  // 事件处理函数
+  bindViewTap() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad() {
+    
+  },
+})
