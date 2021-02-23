7d553b7b0ba323d674ff9535678b39b35f3c0ddb
taomiaoer <taomiaoer@163.com>
2021-02-23 09:14:16 +0800

    新盖

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,8 +1,8 @@
 /*
  * @Author: your name
  * @Date: 2021-02-07 09:12:55
- * @LastEditTime: 2021-02-09 15:29:04
- * @LastEditors: sueRimn
+ * @LastEditTime: 2021-02-22 15:50:23
+ * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \Scooter\pages\index\index.js
  */
@@ -17,7 +17,22 @@ Page({
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     active: 1,
+    
   },
+  
+
+  calling:function(){
+    wx.makePhoneCall({
+      phoneNumber: '12345678900', //此号码并非真实电话号码，仅用于测试
+      success:function(){
+        console.log("拨打电话成功！")
+      },
+      fail:function(){
+        console.log("拨打电话失败！")
+      }
+    })
+  },
+
   onReady: function (e) {
     
   },
