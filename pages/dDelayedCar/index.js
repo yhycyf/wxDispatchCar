b8cc14981358dd8cef383c8463dd27c6d008e1a3
yf <yf@maxdeMacBook-Pro.local>
2021-03-01 18:49:23 +0800

    延时还车

--- pages/dDelayedCar/index.js
+++ pages/dDelayedCar/index.js
@@ -1,8 +1,8 @@
 /*
  * @Author: your name
  * @Date: 2021-02-07 09:12:55
- * @LastEditTime: 2021-02-08 16:40:22
- * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2021-03-01 18:37:35
+ * @LastEditors: sueRimn
  * @Description: In User Settings Edit
  * @FilePath: \Scooter\pages\index\index.js
  */
@@ -18,10 +18,13 @@ Page({
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     active: 1,
     value: '',
+    radio: 0,
   },
-  onChange(event) {
-    // event.detail 为当前输入的值
-    console.log(event.detail);
+  // 单选框
+  onChangeRadio(event) {
+    this.setData({
+      radio: event.detail,
+    });
   },
   // 事件处理函数
   bindViewTap() {
