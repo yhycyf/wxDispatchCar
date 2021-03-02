06ba2fe60e545be5e2dd4986b6e7eb3ebda6ab64
yf <yf@maxdeMacBook-Pro.local>
2021-03-02 18:39:13 +0800

    接送车

---
+++ component/list/index.js
@@ -0,0 +1,64 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: sueRimn
+ * @Date: 2020-12-19 14:14:55
+ * @LastEditors: sueRimn
+ * @LastEditTime: 2021-03-02 14:39:17
+ */
+
+
+const app = getApp();
+Component({
+  options: {
+    multipleSlots: true // 在组件定义时的选项中启用多slot支持
+  },
+  observers: {
+    
+  },
+
+  // 属性定义（详情参见下文）
+  properties: {
+    type: {
+      type: String,
+      value: ''
+    },
+    
+  },
+
+  data: {
+    show: false,
+    currentDate: '12:00',
+  }, // 私有数据，可用于模板渲染
+
+  lifetimes: {
+    // 生命周期函数，可以为函数，或一个在methods段中定义的方法名
+    attached: function() {
+      console.log('attached');
+      // this.getLocation();
+    },
+    moved: function() { },
+    created: function() {
+      console.log('created')
+    },
+    detached: function() {},
+    ready: function() {
+      
+    }
+  },
+
+  pageLifetimes: {
+    // 组件所在页面的生命周期函数
+    show: function() { 
+      console.log('show');
+      
+    },
+    hide: function() { },
+    resize: function() { }
+  },
+
+  methods: {
+    
+  }
+
+});
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,8 +1,8 @@
 /*
  * @Author: your name
  * @Date: 2021-02-07 09:12:55
- * @LastEditTime: 2021-02-25 13:16:09
- * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2021-03-02 17:06:58
+ * @LastEditors: sueRimn
  * @Description: In User Settings Edit
  * @FilePath: \Scooter\pages\index\index.js
  */
@@ -16,7 +16,7 @@ Page({
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    active: 1,
+    active: 2,
     
   },
   
