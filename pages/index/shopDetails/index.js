6fb6d45855ebe395018f67e770d7e83fcf1c813e
yf <yf@maxdeMacBook-Pro.local>
2021-03-04 15:43:48 +0800

    店铺详情

---
+++ pages/index/shopDetails/index.js
@@ -0,0 +1,24 @@
+/*
+ * @Author: your name
+ * @Date: 2021-02-07 09:12:55
+ * @LastEditTime: 2021-03-04 14:14:00
+ * @LastEditors: sueRimn
+ * @Description: In User Settings Edit
+ * @FilePath: \Scooter\pages\index\index.js
+ */
+// index.js
+// 获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    starValue: 4.5
+  },
+
+  onReady: function (e) {
+    
+  },
+  onLoad() {
+    
+  },
+})
