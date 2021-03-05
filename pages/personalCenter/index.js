0f2bee5b35fb1eff289f04b9db97632c12666784
taomiaoer <taomiaoer@163.com>
2021-03-05 17:07:16 +0800

    新增

---
+++ pages/personalCenter/index.js
@@ -0,0 +1,43 @@
+/*
+ * @Author: your name
+ * @Date: 2021-02-07 09:12:55
+ * @LastEditTime: 2021-03-05 11:47:42
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \Scooter\pages\index\index.js
+ */
+// index.js
+// 获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: 'Helelo World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    active: 1,
+    value: '',
+    radio: 0,
+  },
+  payment() {
+    wx.navigateTo({
+      url: '/pages/dDelayedCar/payment/index'
+    })
+  },
+  // 单选框
+  onChangeRadio(event) {
+    this.setData({
+      radio: event.detail,
+    });
+  },
+  // 事件处理函数
+  bindViewTap() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad() {
+    
+  },
+})
